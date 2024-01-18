f90a871d0578f00601b1f1243f94bcf003e798c6
Mohammed Anjoom <anjoomvkkl@gmail.com>
2024-01-18 23:18:34 +0530

    Jh

---
+++ travello/travelapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Place, Team
+
+
+# Create your views here.
+def demo(request):
+    obj = Place.objects.all()
+    obj2 = Team.objects.all()
+    return render(request, "index.html", {'result': obj, 'team': obj2})
